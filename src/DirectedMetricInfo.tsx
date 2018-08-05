c6e71bc84f1c25e09a5a6404bec0ef378a346063
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-08-04 21:42:07 -0400

    Extract card metrics into separate component

---
+++ src/DirectedMetricInfo.tsx
@@ -0,0 +1,39 @@
+/* tslint:disable jsx-no-multiline-js */
+import Card from '@material-ui/core/Card/Card'
+import CardContent from '@material-ui/core/CardContent/CardContent'
+import Table from '@material-ui/core/Table/Table'
+import TableBody from '@material-ui/core/TableBody/TableBody'
+import TableCell from '@material-ui/core/TableCell/TableCell'
+import TableRow from '@material-ui/core/TableRow/TableRow'
+import Typography from '@material-ui/core/Typography'
+import * as React from 'react'
+
+export interface IDirectedMetricInfoProps {
+  title: string
+  rows: IDirectedMetricInfoRow[]
+}
+
+export interface IDirectedMetricInfoRow {
+  label: string
+  value: string
+}
+
+const DirectedMetricInfo: React.SFC<IDirectedMetricInfoProps> = ({ title, rows }) => (
+  <Card raised={true}>
+    <CardContent>
+      <Typography variant='subheading' gutterBottom={true}>{title}</Typography>
+      <Table>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow>
+              <TableCell variant='head'>{row.label}</TableCell>
+              <TableCell numeric={true}>{row.value}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </CardContent>
+  </Card>
+)
+
+export default DirectedMetricInfo
--- src/PlayerPanel.tsx
+++ src/PlayerPanel.tsx
@@ -1,19 +1,38 @@
 /* tslint:disable jsx-no-multiline-js */
-import Card from '@material-ui/core/Card/Card'
-import CardContent from '@material-ui/core/CardContent/CardContent'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import Grid from '@material-ui/core/Grid/Grid'
-import Table from '@material-ui/core/Table/Table'
-import TableBody from '@material-ui/core/TableBody/TableBody'
-import TableCell from '@material-ui/core/TableCell/TableCell'
-import TableRow from '@material-ui/core/TableRow/TableRow'
 import Typography from '@material-ui/core/Typography'
 import ExpandMore from '@material-ui/icons/ExpandMore'
 import { numberFormat } from 'highcharts'
 import * as React from 'react'
+import DirectedMetricInfo from './DirectedMetricInfo'
 import { getPrimaryResourceBySpecialization } from './specializations'
+
+const buildErrorString = (
+  confidence: number,
+  meanStdDev: number,
+  mean: number
+) =>
+  numberFormat(confidence * meanStdDev, 2) +
+  ' / ' +
+  numberFormat((confidence * meanStdDev * 100) / mean, 3) +
+  '%'
+
+const buildMetricPerResourceString = (
+  meanMetric: number,
+  resourceLost: { [resourceName: string]: ISampleData },
+  specialization: ClassSpecialization
+) =>
+  (resourceLost &&
+    getPrimaryResourceBySpecialization(specialization) &&
+    numberFormat(
+      meanMetric /
+      resourceLost[getPrimaryResourceBySpecialization(specialization)].mean,
+      2
+    )) ||
+  '0'
 
 export interface IPlayerPanelProps {
   player: IActor
@@ -27,147 +46,175 @@ const PlayerPanel: React.SFC<IPlayerPanelProps> = ({ player, confidence }) => (
     </ExpansionPanelSummary>
     <ExpansionPanelDetails>
       <Grid container={true} spacing={16}>
-        {player.collected_data.dps && (
+        {player.collected_data.dps &&
+        player.collected_data.dpse && (
           <Grid item={true}>
-            <Card raised={true}>
-              <CardContent>
-                <Typography variant='subheading' gutterBottom={true}>
-                  Outgoing Damage
-                </Typography>
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell variant='head'>DPS</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.dps && numberFormat(player.collected_data.dps.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DPSe</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.dpse && numberFormat(player.collected_data.dpse.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DPS Error</TableCell>
-                      <TableCell numeric={true}>
-                        {numberFormat(confidence * player.collected_data.dps.mean_std_dev, 2)}
-                        {' / '}
-                        {numberFormat(confidence * player.collected_data.dps.mean_std_dev * 100 / player.collected_data.dps.mean, 3)}%
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DPS Range</TableCell>
-                      <TableCell numeric={true}>0</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DPR</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.resource_lost && getPrimaryResourceBySpecialization(player.specialization) && numberFormat(player.collected_data.dmg.mean / player.collected_data.resource_lost[getPrimaryResourceBySpecialization(player.specialization)].mean, 2) || 0}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
+            <DirectedMetricInfo
+              title='Outgoing Damage'
+              rows={[
+                {
+                  label: 'DPS',
+                  value: numberFormat(player.collected_data.dps.mean)
+                },
+                {
+                  label: 'DPSe',
+                  value: numberFormat(player.collected_data.dpse.mean)
+                },
+                {
+                  label: 'DPS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.dps.mean_std_dev,
+                    player.collected_data.dps.mean
+                  )
+                },
+                { label: 'DPS Range', value: '0' },
+                {
+                  label: 'DPR',
+                  value: buildMetricPerResourceString(
+                    player.collected_data.dps.mean,
+                    player.collected_data.resource_lost,
+                    player.specialization
+                  )
+                }
+              ]}
+            />
           </Grid>
         )}
 
         {player.collected_data.dtps && (
           <Grid item={true}>
-            <Card raised={true}>
-              <CardContent>
-                <Typography variant='subheading' gutterBottom={true}>
-                  Incoming Damage
-                </Typography>
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell variant='head'>DTPS</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.dtps && numberFormat(player.collected_data.dtps.mean / player.collected_data.fight_length.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DTPS Error</TableCell>
-                      <TableCell numeric={true}>
-                        {numberFormat(confidence * player.collected_data.dtps.mean_std_dev, 2)}
-                        {' / '}
-                        {numberFormat(confidence * player.collected_data.dtps.mean_std_dev * 100 / player.collected_data.dtps.mean, 3)}%
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>DTPS Range</TableCell>
-                      <TableCell numeric={true}>0</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
+            <DirectedMetricInfo
+              title='Incoming Damage'
+              rows={[
+                {
+                  label: 'DTPS',
+                  value: numberFormat(player.collected_data.dtps.mean)
+                },
+                {
+                  label: 'DTPS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.dtps.mean_std_dev,
+                    player.collected_data.dtps.mean
+                  )
+                },
+                { label: 'DPS Range', value: '0' }
+              ]}
+            />
           </Grid>
         )}
 
-        {player.collected_data.hps && (
+        {player.collected_data.hps &&
+        player.collected_data.hpse && (
           <Grid item={true}>
-            <Card raised={true}>
-              <CardContent>
-                <Typography variant='subheading' gutterBottom={true}>
-                  Outgoing Healing
-                </Typography>
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell variant='head'>HPS</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.hps && numberFormat(player.collected_data.hps.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HPSe</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.hpse && numberFormat(player.collected_data.hpse.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HPS Error</TableCell>
-                      <TableCell numeric={true}>
-                        {numberFormat(confidence * player.collected_data.hps.mean_std_dev, 2)}
-                        {' / '}
-                        {numberFormat(confidence * player.collected_data.hps.mean_std_dev * 100 / player.collected_data.hps.mean, 3)}%
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HPS Range</TableCell>
-                      <TableCell numeric={true}>0</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HPR</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.resource_lost && player.collected_data.heal && getPrimaryResourceBySpecialization(player.specialization) && numberFormat(player.collected_data.heal.mean / player.collected_data.resource_lost[getPrimaryResourceBySpecialization(player.specialization)].mean, 2) || 0}</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
+            <DirectedMetricInfo
+              title='Outgoing Healing'
+              rows={[
+                {
+                  label: 'HPS',
+                  value: numberFormat(player.collected_data.hps.mean)
+                },
+                {
+                  label: 'HPSe',
+                  value: numberFormat(player.collected_data.hpse.mean)
+                },
+                {
+                  label: 'HPS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.hps.mean_std_dev,
+                    player.collected_data.hps.mean
+                  )
+                },
+                { label: 'HPS Range', value: '0' },
+                {
+                  label: 'HPR',
+                  value: buildMetricPerResourceString(
+                    player.collected_data.hps.mean,
+                    player.collected_data.resource_lost,
+                    player.specialization
+                  )
+                }
+              ]}
+            />
           </Grid>
         )}
 
         {player.collected_data.htps && (
           <Grid item={true}>
-            <Card raised={true}>
-              <CardContent>
-                <Typography variant='subheading' gutterBottom={true}>
-                  Incoming Damage
-                </Typography>
-                <Table>
-                  <TableBody>
-                    <TableRow>
-                      <TableCell variant='head'>HTPS</TableCell>
-                      <TableCell numeric={true}>{player.collected_data.htps && numberFormat(player.collected_data.htps.mean / player.collected_data.fight_length.mean, 0)}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HTPS Error</TableCell>
-                      <TableCell numeric={true}>
-                        {numberFormat(confidence * player.collected_data.htps.mean_std_dev, 2)}
-                        {' / '}
-                        {numberFormat(confidence * player.collected_data.htps.mean_std_dev * 100 / player.collected_data.htps.mean, 3)}%
-                      </TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell variant='head'>HTPS Range</TableCell>
-                      <TableCell numeric={true}>0</TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </CardContent>
-            </Card>
+            <DirectedMetricInfo
+              title='Incoming Healing'
+              rows={[
+                {
+                  label: 'HTPS',
+                  value: numberFormat(player.collected_data.htps.mean)
+                },
+                {
+                  label: 'HTPS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.htps.mean_std_dev,
+                    player.collected_data.htps.mean
+                  )
+                },
+                { label: 'HTPS Range', value: '0' }
+              ]}
+            />
+          </Grid>
+        )}
+
+        {player.collected_data.aps && (
+          <Grid item={true}>
+            <DirectedMetricInfo
+              title='Outgoing Absorbs'
+              rows={[
+                {
+                  label: 'APS',
+                  value: numberFormat(player.collected_data.aps.mean)
+                },
+                {
+                  label: 'APS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.aps.mean_std_dev,
+                    player.collected_data.aps.mean
+                  )
+                },
+                { label: 'APS Range', value: '0' },
+                {
+                  label: 'APR',
+                  value: buildMetricPerResourceString(
+                    player.collected_data.aps.mean,
+                    player.collected_data.resource_lost,
+                    player.specialization
+                  )
+                }
+              ]}
+            />
+          </Grid>
+        )}
+
+        {player.collected_data.atps && (
+          <Grid item={true}>
+            <DirectedMetricInfo
+              title='Incoming Absorbs'
+              rows={[
+                {
+                  label: 'ATPS',
+                  value: numberFormat(player.collected_data.atps.mean)
+                },
+                {
+                  label: 'ATPS Error',
+                  value: buildErrorString(
+                    confidence,
+                    player.collected_data.atps.mean_std_dev,
+                    player.collected_data.atps.mean
+                  )
+                },
+                { label: 'ATPS Range', value: '0' }
+              ]}
+            />
           </Grid>
         )}
       </Grid>
