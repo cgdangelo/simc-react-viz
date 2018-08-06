f05e34d8768c46d52575daaf320ddaa4b77e63b6
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-08-06 10:51:28 -0400

    Update declarations for highcharts react

--- highcharts-react-official.d.ts
+++ highcharts-react-official.d.ts
@@ -1,1 +1,12 @@
-declare module 'highcharts-react-official'
+import Highcharts = require('highcharts')
+import React = require('react')
+
+declare module 'highcharts-react-official' {
+  export interface IHighchartsReactProps {
+    highcharts: Highcharts.Static
+    options: Highcharts.Options
+  }
+
+  export default class HighchartsReact extends React.Component<IHighchartsReactProps> {
+  }
+}
