5cce72db54ca4a3ea7557c030b1eed053b06d97e
Charles D'Angelo <c.g.dangelo@gmail.com>
2018-08-17 16:38:18 -0400

    Allow user to set start of relative difference calculations

--- src/RaidSummary/StackedActorChart.js
+++ src/RaidSummary/StackedActorChart.js
@@ -5,78 +5,90 @@ import * as React from 'react'
 
 const {numberFormat} = Highcharts
 
-export const StackedActorChart = ({boxPlot, series: {data, name, precision = 0}, title}) => {
-  const benchMetric = data.slice(-1).shift().y
+class StackedActorChart extends React.PureComponent {
+  static propTypes = {
+    boxPlot: PropTypes.array,
 
-  const chartOptions = {
-    chart: {
-      height: Math.max(data.length * 50, 300)
-    },
-    series: [
-      {
-        data,
-        dataLabels: {
-          format: `{point.y:,.${precision}f}`
-        },
-        name,
-        type: 'bar'
-      }
-    ],
-    title: {
-      text: title
-    },
-    xAxis: {
-      categories: data.map(bar => {
-        if (bar.y === benchMetric) {
-          return bar.name
+    series: PropTypes.shape({
+      data: PropTypes.arrayOf(PropTypes.shape({
+        color: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        y: PropTypes.number.isRequired
+      })).isRequired,
+
+      name: PropTypes.string.isRequired,
+      precision: PropTypes.number
+    }).isRequired,
+
+    title: PropTypes.string.isRequired
+  }
+
+  state = {
+    relativeIndex: this.props.series.data.length - 1
+  }
+
+  render () {
+    const {boxPlot, series: {data, name, precision = 0}, title} = this.props
+    const relativeMetric = data[this.state.relativeIndex].y
+
+    const chartOptions = {
+      chart: {
+        height: Math.max(data.length * 50, 300)
+      },
+      series: [
+        {
+          data,
+          dataLabels: {
+            format: `{point.y:,.${precision}f}`
+          },
+          events: {
+            click: (event) => {
+              this.setState({relativeIndex: event.point.x})
+            }
+          },
+          name,
+          type: 'bar'
         }
+      ],
+      title: {
+        text: title
+      },
+      xAxis: {
+        categories: data.map(bar => {
+          if (bar.y === relativeMetric) {
+            return bar.name
+          }
 
-        const relativeDifference = (bar.y - benchMetric) / benchMetric * 100
+          const relativeDifference = (bar.y > relativeMetric ? (bar.y - relativeMetric) / relativeMetric : (relativeMetric - bar.y) / bar.y) * 100
 
-        return `${bar.name} (${numberFormat(relativeDifference)}%)`
-      }),
-      labels: {
-        formatter () {
-          return `<span style='color: ${data[this.pos].color}'>${this.value}</span>`
+          return `${bar.name} (${bar.y < relativeMetric ? '-' : ''}${numberFormat(relativeDifference)}%)`
+        }),
+        labels: {
+          formatter () {
+            return `<span style='color: ${data[this.pos].color}'>${this.value}</span>`
+          }
         }
       }
     }
+
+    if (boxPlot) {
+      chartOptions.series.push({
+        data: boxPlot,
+        name,
+        tooltip: {
+          pointFormat: `Maximum: <b>{point.high}</b><br/>Upper quartile: <b>{point.q3}</b><br/>Mean: <b>{point.mean:,.1f}</b><br/>Median: <b>{point.median}</b><br/>Lower quartile: <b>{point.q1}</b><br/>Minimum: <b>{point.low}</b><br/>`
+        },
+        type: 'boxplot'
+      })
+    }
+
+    return (
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={chartOptions}
+      />
+    )
   }
-
-  if (boxPlot) {
-    chartOptions.series.push({
-      data: boxPlot,
-      name,
-      tooltip: {
-        pointFormat: `Maximum: <b>{point.high}</b><br/>Upper quartile: <b>{point.q3}</b><br/>Mean: <b>{point.mean:,.1f}</b><br/>Median: <b>{point.median}</b><br/>Lower quartile: <b>{point.q1}</b><br/>Minimum: <b>{point.low}</b><br/>`
-      },
-      type: 'boxplot'
-    })
-  }
-
-  return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={chartOptions}
-    />
-  )
-}
-
-StackedActorChart.propTypes = {
-  boxPlot: PropTypes.array,
-
-  series: PropTypes.shape({
-    data: PropTypes.arrayOf(PropTypes.shape({
-      color: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      y: PropTypes.number.isRequired
-    })).isRequired,
-
-    name: PropTypes.string.isRequired,
-    precision: PropTypes.number
-  }).isRequired,
-
-  title: PropTypes.string.isRequired
 }
 
 export default StackedActorChart
